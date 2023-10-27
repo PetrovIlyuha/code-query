63f49dfb5cae4fd18cfdd36ebe62ef2233c858b1
Petrov Ilya <dnamix1@gmail.com>
2023-10-27 15:42:28 +0300

    add fonts and metadata to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,30 @@
 import { ClerkProvider } from '@clerk/nextjs'
+import { Metadata } from 'next'
+import { Inter, Space_Grotesk } from 'next/font/google'
 import React from 'react'
+
+import './globals.css'
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter'
+})
+
+const grotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk'
+})
+
+export const metadata: Metadata = {
+  title: 'CodeQuery',
+  description:
+    'CodeQuery is the ultimate Q&A platform, designed to foster trust among seasoned developers while warmly welcoming beginners on their coding journey. Find expert answers, guidance, and a supportive community to empower your programming success.',
+  icons: {
+    icon: '/assets/images/site_logo.png'
+  }
+}
 
 export default function RootLayout({
   children
@@ -7,9 +32,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink: 'primary-text-gradient hover:text-primary-500'
+        }
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${grotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   )
